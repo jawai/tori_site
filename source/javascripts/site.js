14cd3c244db716ce28837e6bd5a351b07b5d6d27
Jason Wai <jason.a.wai@gmail.com>
2017-09-01 19:23:11 -0400

    fixed navigation hamburger with js

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,16 +1,18 @@
 // This is where it all goes :)
 //= require jquery
 //= require tree .
+
 $(document).ready(function() {
   (function blink() {
     $('.blink_me').fadeOut(500).fadeIn(500, blink);
   })();
-});
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+  (function() {
+      var burger = document.querySelector('.nav-toggle');
+      var menu = document.querySelector('.nav-menu');
+      burger.addEventListener('click', function() {
+          burger.classList.toggle('is-active');
+          menu.classList.toggle('is-active');
+      });
+  })();
+});
