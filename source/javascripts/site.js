95840488589c60c29f9bda395a5baba0f9da512b
Jason Wai <jason.a.wai@gmail.com>
2017-09-03 19:20:00 -0400

    add headroom js for scroll down invisible nav and scroll up visible

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -15,4 +15,10 @@ $(document).ready(function() {
           menu.classList.toggle('is-active');
       });
   })();
+
+  var myElement = document.querySelector("header");
+  // construct an instance of Headroom, passing the element
+  var headroom  = new Headroom(myElement);
+  // initialise
+  headroom.init();
 });
